0eac1ce09c18e5af4d8bb05343b8792da00b6531
Neoggg <thegitem@hotmail.com>
2021-09-09 17:06:40 +0800

    fixbug

--- lineBot.php
+++ lineBot.php
@@ -14,8 +14,8 @@ require_once('forLine/LineJson.php');
 $channelSecret =  '64f2e4b2431a448b2c872f5c58a201a9';
 $channelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU=';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelSecret);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelAccessToken]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 $data = $this->menu();
 
 $richMenuBuilder = new \LINE\LINEBot\RichMenuBuilder($data['size'], $data['selected'], $data['name'], $data['chatBarText'], $data['areas']);
