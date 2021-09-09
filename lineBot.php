e6a9b13f7e4b56805be48a54b650ec8c6fbe931d
Neoggg <thegitem@hotmail.com>
2021-09-09 11:36:25 +0800

    token

--- lineBot.php
+++ lineBot.php
@@ -10,12 +10,12 @@ date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
 require_once('LINEBotTiny.php');
 
 //設定Token 
-$ChannelSecret = '64f2e4b2431a448b2c872f5c58a201a9'; 
+$ChannelSecret =  '64f2e4b2431a448b2c872f5c58a201a9'; 
 $ChannelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU='; 
  
 //讀取資訊 
 $HttpRequestBody = file_get_contents('php://input'); 
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+$client = new LINEBotTiny('b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU=', '64f2e4b2431a448b2c872f5c58a201a9');
 foreach ($client->parseEvents() as $event) {
     $client->replyMessage(array(
         'replyToken' => $event['replyToken'],
