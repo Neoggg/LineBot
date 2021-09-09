b92e3c464eb84f866d1e23cbdce6c5e740b87097
許家齊 <neogg@xujiaqis-MacBook-Pro.local>
2021-09-09 22:59:33 +0800

    testmor

--- lineBot.php
+++ lineBot.php
@@ -12,34 +12,34 @@ require_once('forLine/LineJson.php');
 require_once('vendor/autoload.php');
 
 $HttpRequestBody = file_get_contents('php://input');
-file_put_contents('php://stderr', $HttpRequestBody);
+// file_put_contents('php://stderr', $HttpRequestBody);
 
 //設定Token 
 $channelSecret =  '64f2e4b2431a448b2c872f5c58a201a9';
 $channelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU=';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-$data = $this->menu();
-$richMenuBuilder = new \LINE\LINEBot\RichMenuBuilder($data['size'], $data['selected'], $data['name'], $data['chatBarText'], $data['areas']);
-$response = $bot->createRichMenu($richMenuBuilder);
-$status = $response->getHTTPStatus();
-if ($status == 200) {
-	$content = $response->getJSONDecodedBody();
-	$richMenuId = $content['richMenuId'];
-	$imagePath = __DIR__ . '/el4.jpg';
-	$contentType = 'image/jpeg';
-	$imgResponse = $bot->uploadRichMenuImage($richMenuId, $imagePath, $contentType);
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/bot/user/all/richmenu/' . $richMenuId);
-	curl_setopt($ch, CURLOPT_POST, true);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-	curl_setopt($ch, CURLOPT_HTTPHEADER, [
-		'Content-Type: application/json',
-		'Authorization: Bearer ' . $channelAccessToken
-	]);
-	curl_close($ch);
-}
+// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+// $data = $this->menu();
+// $richMenuBuilder = new \LINE\LINEBot\RichMenuBuilder($data['size'], $data['selected'], $data['name'], $data['chatBarText'], $data['areas']);
+// $response = $bot->createRichMenu($richMenuBuilder);
+// $status = $response->getHTTPStatus();
+// if ($status == 200) {
+// 	$content = $response->getJSONDecodedBody();
+// 	$richMenuId = $content['richMenuId'];
+// 	$imagePath = __DIR__ . '/el4.jpg';
+// 	$contentType = 'image/jpeg';
+// 	$imgResponse = $bot->uploadRichMenuImage($richMenuId, $imagePath, $contentType);
+// 	$ch = curl_init();
+// 	curl_setopt($ch, CURLOPT_URL, 'https://api.line.me/v2/bot/user/all/richmenu/' . $richMenuId);
+// 	curl_setopt($ch, CURLOPT_POST, true);
+// 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+// 	curl_setopt($ch, CURLOPT_HTTPHEADER, [
+// 		'Content-Type: application/json',
+// 		'Authorization: Bearer ' . $channelAccessToken
+// 	]);
+// 	curl_close($ch);
+// }
 
 //讀取資訊 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
@@ -73,6 +73,6 @@ foreach ($client->parseEvents() as $event) {
 }
 
 //輸出
-// error_log("error_test"); 
+error_log(); 
 // file_put_contents('php://stderr', json_encode($dataEven));
-// file_put_contents('php://stderr', $HttpRequestBody);
+file_put_contents('php://stderr', $HttpRequestBody);
