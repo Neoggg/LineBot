1d32b70cd27228db29c2abb2f4e569c1b46cf6b4
Neoggg <thegitem@hotmail.com>
2021-09-09 10:50:24 +0800

    add log

--- lineBot.php
+++ lineBot.php
@@ -14,7 +14,7 @@ $ChannelSecret = '64f2e4b2431a448b2c872f5c58a201a9';
 $ChannelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU='; 
  
 //讀取資訊 
-// $HttpRequestBody = file_get_contents('php://input'); 
+$HttpRequestBody = file_get_contents('php://input'); 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
   switch ($event['type']) {
@@ -34,4 +34,4 @@ foreach ($client->parseEvents() as $event) {
 }
 
 //輸出 
-// file_put_contents('log.txt', $HttpRequestBody); 
+file_put_contents('log.txt', $HttpRequestBody); 
