8a27a2d78b331d0fbc1c51051285894825ae622f
許家齊 <neogg@xujiaqis-MacBook-Pro.local>
2021-09-09 23:04:40 +0800

    add

---
+++ vendor/linecorp/line-bot-sdk/src/LINEBot/RichMenuBuilder/RichMenuAreaActionBuilder.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+namespace LINE\LINEBot\RichMenuBuilder;
+
+use LINE\LINEBot\TemplateActionBuilder;
+
+/**
+ * A builder class for area of rich menu area object.
+ *
+ * @package LINE\LINEBot\RichMenuBuilder
+ */
+class RichMenuAreaActionBuilder implements TemplateActionBuilder
+{
+    /** @var string */
+    private $type;
+    /** @var string */
+    private $data;
+    /** @var string */
+    // private $text;
+
+    public function __construct($type, $data)
+    {
+        $this->type = $type;
+        $this->data = $data;
+        // $this->text = $text;
+    }
+
+    /**
+     * Builds imagemap area structure.
+     *
+     * @return array Built area structure.
+     */
+    public function buildTemplateAction()
+    {
+        return [
+            'type' => $this->type,
+            'data' => $this->data,
+            // 'text' => $this->text,
+        ];
+    }
+}
