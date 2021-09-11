bbe7bdc1f7f4db218410ec4ff08707ad5d31d504
Neoggg <thegitem@hotmail.com>
2021-09-11 14:03:40 +0800

    test3

--- lineBot.php
+++ lineBot.php
@@ -67,7 +67,7 @@ foreach ($client->parseEvents() as $event) {
 							"type" => "text", // ①
 							"text" => "Select your favorite food category or send me your location!",
 							'quickReply' => array(
-								"item" => array(
+								"items" => array(
 									array(
 										'type' => 'action', //訊息類型 (文字)
 										"action" => array(
