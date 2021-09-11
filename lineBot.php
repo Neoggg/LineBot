8446974311a693dc881fc6c2bb04bec7a326d757
Neoggg <thegitem@hotmail.com>
2021-09-11 16:37:04 +0800

    test

--- lineBot.php
+++ lineBot.php
@@ -1,7 +1,7 @@
 <?php
 // require_once('LINEBotTiny.php');
 require_once('forLine/LineJson.php');
-require_once(__DIR__ . '/autoload.php');
+require_once(__DIR__ . '/vendor/autoload.php');
 
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
