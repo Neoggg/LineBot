bcf8b2b9c3db87b75845574d463739ce3e41c1ca
Neoggg <thegitem@hotmail.com>
2021-09-11 14:36:01 +0800

    testcomposer

--- lineBot.php
+++ lineBot.php
@@ -7,7 +7,7 @@
 
 // date_default_timezone_set("Asia/Taipei"); //設定時區為台北時區
 
-require_once('LINEBotTiny.php');
+// require_once('LINEBotTiny.php');
 require_once('forLine/LineJson.php');
 require_once('vendor/autoload.php');
 
@@ -18,9 +18,9 @@ file_put_contents('php://stderr', $HttpRequestBody);
 $channelSecret =  '64f2e4b2431a448b2c872f5c58a201a9';
 $channelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU=';
 
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
-// file_put_contents('php://stderr', json_encode($httpClient));
-// $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+file_put_contents('php://stderr', json_encode($httpClient));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 // $LineJson = new LineJson();
 // $data = $LineJson->menu();
 // file_put_contents('php://stderr', json_encode($data));
@@ -49,63 +49,63 @@ $channelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fK
 // }
 
 //讀取資訊 
-$client = new LINEBotTiny($channelAccessToken, $channelSecret);
-$dataEven = $client->parseEvents();
-foreach ($client->parseEvents() as $event) {
-	switch ($event['type']) {
-		case 'message': //訊息觸發
-			$message = $event['message'];
-			if (strtolower($message['text']) == "test") {
-				$client->replyMessage(array(
-					'replyToken' => $event['replyToken'],
-					'messages' => array(
-						array(
-							'type' => 'text', //訊息類型 (文字)
-							'text' => '金罵勒test' //回覆訊息
-						),
-					),
-				));
-			} elseif (strtolower($message['text']) == 'meun') {
-				$client->replyMessage(array(
-					'replyToken' => $event['replyToken'],
-					'messages' => array(
-						"type" => "text",
-						"text" => "Select your favorite food category or send me your location!",
-						'quickReply' => array(
-							"items" => array(
-								array(
-									'type' => 'action', //訊息類型 (文字)
-									"action" => array(
-										"type" => "message",
-										"label" => "A.台北",
-										"text" => "台北"
-									)
-								),
-								array(
-									'type' => 'action', //訊息類型 (文字)
-									"action" => array(
-										"type" => "message",
-										"label" => "b.台南",
-										"text" => "台南"
-									)
-								),
-							)
-						),
-					)
-				));
-			} else {
-				$client->replyMessage(array(
-					'replyToken' => $event['replyToken'],
-					'messages' => array(
-						array(
-							'type' => 'text', //訊息類型 (文字)
-							'text' => '母湯歐北怕' //回覆訊息
-						)
-					)
-				));
-			}
-	}
-}
+// $client = new LINEBotTiny($channelAccessToken, $channelSecret);
+// $dataEven = $client->parseEvents();
+// foreach ($client->parseEvents() as $event) {
+// 	switch ($event['type']) {
+// 		case 'message': //訊息觸發
+// 			$message = $event['message'];
+// 			if (strtolower($message['text']) == "test") {
+// 				$client->replyMessage(array(
+// 					'replyToken' => $event['replyToken'],
+// 					'messages' => array(
+// 						array(
+// 							'type' => 'text', //訊息類型 (文字)
+// 							'text' => '金罵勒test' //回覆訊息
+// 						),
+// 					),
+// 				));
+// 			} elseif (strtolower($message['text']) == 'meun') {
+// 				$client->replyMessage(array(
+// 					'replyToken' => $event['replyToken'],
+// 					'messages' => array(
+// 						"type" => "text",
+// 						"text" => "Select your favorite food category or send me your location!",
+// 						'quickReply' => array(
+// 							"items" => array(
+// 								array(
+// 									'type' => 'action', //訊息類型 (文字)
+// 									"action" => array(
+// 										"type" => "message",
+// 										"label" => "A.台北",
+// 										"text" => "台北"
+// 									)
+// 								),
+// 								array(
+// 									'type' => 'action', //訊息類型 (文字)
+// 									"action" => array(
+// 										"type" => "message",
+// 										"label" => "b.台南",
+// 										"text" => "台南"
+// 									)
+// 								),
+// 							)
+// 						),
+// 					)
+// 				));
+// 			} else {
+// 				$client->replyMessage(array(
+// 					'replyToken' => $event['replyToken'],
+// 					'messages' => array(
+// 						array(
+// 							'type' => 'text', //訊息類型 (文字)
+// 							'text' => '母湯歐北怕' //回覆訊息
+// 						)
+// 					)
+// 				));
+// 			}
+// 	}
+// }
 
 //輸出
 // error_log(); 
