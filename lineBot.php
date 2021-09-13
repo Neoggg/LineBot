755719f22cc15494958d575214dc0bf774af15d7
Neoggg <thegitem@hotmail.com>
2021-09-13 11:10:20 +0800

    test

--- lineBot.php
+++ lineBot.php
@@ -13,17 +13,17 @@ $channelSecret =  '64f2e4b2431a448b2c872f5c58a201a9';
 $channelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU=';
 
 $httpClient = new CurlHTTPClient($channelAccessToken);
-// $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
-file_put_contents('php://stderr', serialize($httpClient));
-exit;
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+// file_put_contents('php://stderr', serialize($httpClient));
+// exit;
 // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
-// $LineJson = new LineJson();
-// $data = $LineJson->menu();
-// file_put_contents('php://stderr', json_encode($data));
-// $richMenuBuilder = new \LINE\LINEBot\RichMenuBuilder($data['size'], $data['selected'], $data['name'], $data['chatBarText'], $data['areas']);
+$LineJson = new LineJson();
+$data = $LineJson->menu();
+$richMenuBuilder = new \LINE\LINEBot\RichMenuBuilder($data['size'], $data['selected'], $data['name'], $data['chatBarText'], $data['areas']);
 // file_put_contents('php://stderr', json_encode($richMenuBuilder));
-// $response = $bot->createRichMenu($richMenuBuilder);
-// file_put_contents('php://stderr', json_encode($response));
+$response = $bot->createRichMenu($richMenuBuilder);
+file_put_contents('php://stderr', serialize($response));
+exit;
 // $status = $response->getHTTPStatus();
 // if ($status == 200) {
 // 	$content = $response->getJSONDecodedBody();
