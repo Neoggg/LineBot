66e29a9eb9ca2187e330cd54952f1efa7b667466
Neoggg <thegitem@hotmail.com>
2021-09-14 13:25:47 +0800

    test

--- lineBot.php
+++ lineBot.php
@@ -9,7 +9,9 @@ use \LINE\LINEBot\QuickReplyBuilder\QuickReplyMessageBuilder;
 use \LINE\LINEBot\QuickReplyBuilder\ButtonBuilder\QuickReplyButtonBuilder;
 use \LINE\LINEBot\TemplateActionBuilder\LocationTemplateActionBuilder;
 
+$jsonMessage = '';
 $jsonMap = '{"destination":"U52d37d1d9b625754fa7308ed6af263ef","events":[{"type":"message","message":{"type":"location","id":"14743074306580","latitude":25.028028164750477,"longitude":121.5493593925288,"address":"106台灣台北市大安區敦化南路二段111號"},"timestamp":1631590614516,"source":{"type":"user","userId":"U420aa8a5b3859615a73a08c3f9fa53e2"},"replyToken":"5b7db4bb1a924978918706f76292c7a0","mode":"active"}]}';
+
 $HttpRequestBody = file_get_contents('php://input');
 // file_put_contents('php://stderr', json_encode($HttpRequestBody));
 $linePost = new LinePost($HttpRequestBody);
@@ -25,10 +27,13 @@ $QuickReplyMessageBuilder = new QuickReplyMessageBuilder([
 	new QuickReplyButtonBuilder(new  \LINE\LINEBot\TemplateActionBuilder\CameraTemplateActionBuilder('Camera')),
 	new QuickReplyButtonBuilder(new  \LINE\LINEBot\TemplateActionBuilder\CameraRollTemplateActionBuilder('Camera roll')),
 ]);
-// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('tset', $QuickReplyMessageBuilder);
-// $bot->replyMessage($linePost->getReplyToken(), $textMessageBuilder);
-$pushMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder(json_encode($linePost->getMessage()));
-$bot->pushMessage($linePost->getUserId(), $pushMessageBuilder);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($HttpRequestBody, $QuickReplyMessageBuilder);
+$bot->replyMessage($linePost->getReplyToken(), $textMessageBuilder);
+$messafeInfo = $linePost->getMessage();
+if ($messafeInfo['latitude'] == 'location') {
+	$pushMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messafeInfo['latitude'] . ' ' . $messafeInfo['longitude']);
+	$bot->pushMessage($linePost->getUserId(), $pushMessageBuilder);
+}
 $LineJson = new LineJson();
 $data = $LineJson->menu();
 $richMenuBuilder = new \LINE\LINEBot\RichMenuBuilder($data['size'], $data['selected'], $data['name'], $data['chatBarText'], $data['areas']);
