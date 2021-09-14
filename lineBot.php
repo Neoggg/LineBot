7d612a2c18c76d21fba03890668911d1b8388adf
Neoggg <thegitem@hotmail.com>
2021-09-14 13:28:56 +0800

    test

--- lineBot.php
+++ lineBot.php
@@ -9,7 +9,7 @@ use \LINE\LINEBot\QuickReplyBuilder\QuickReplyMessageBuilder;
 use \LINE\LINEBot\QuickReplyBuilder\ButtonBuilder\QuickReplyButtonBuilder;
 use \LINE\LINEBot\TemplateActionBuilder\LocationTemplateActionBuilder;
 
-$jsonMessage = '';
+$jsonMessage = '{"destination":"U52d37d1d9b625754fa7308ed6af263ef","events":[{"type":"message","message":{"type":"text","id":"14743579319820","text":"..."},"timestamp":1631597207787,"source":{"type":"user","userId":"U420aa8a5b3859615a73a08c3f9fa53e2"},"replyToken":"f223ae75bb764c22a14cc18f72fc1929","mode":"active"}]}';
 $jsonMap = '{"destination":"U52d37d1d9b625754fa7308ed6af263ef","events":[{"type":"message","message":{"type":"location","id":"14743074306580","latitude":25.028028164750477,"longitude":121.5493593925288,"address":"106台灣台北市大安區敦化南路二段111號"},"timestamp":1631590614516,"source":{"type":"user","userId":"U420aa8a5b3859615a73a08c3f9fa53e2"},"replyToken":"5b7db4bb1a924978918706f76292c7a0","mode":"active"}]}';
 
 $HttpRequestBody = file_get_contents('php://input');
@@ -27,10 +27,12 @@ $QuickReplyMessageBuilder = new QuickReplyMessageBuilder([
 	new QuickReplyButtonBuilder(new  \LINE\LINEBot\TemplateActionBuilder\CameraTemplateActionBuilder('Camera')),
 	new QuickReplyButtonBuilder(new  \LINE\LINEBot\TemplateActionBuilder\CameraRollTemplateActionBuilder('Camera roll')),
 ]);
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($HttpRequestBody, $QuickReplyMessageBuilder);
-$bot->replyMessage($linePost->getReplyToken(), $textMessageBuilder);
 $messafeInfo = $linePost->getMessage();
-if ($messafeInfo['latitude'] == 'location') {
+if ($messafeInfo['type'] == 'message') {
+	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messafeInfo['text'], $QuickReplyMessageBuilder);
+	$bot->replyMessage($linePost->getReplyToken(), $textMessageBuilder);
+}
+if ($messafeInfo['type'] == 'location') {
 	$pushMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($messafeInfo['latitude'] . ' ' . $messafeInfo['longitude']);
 	$bot->pushMessage($linePost->getUserId(), $pushMessageBuilder);
 }
@@ -61,28 +63,3 @@ if ($status == 200) {
 exit;
 
 $jsonMap = '{"destination":"U52d37d1d9b625754fa7308ed6af263ef","events":[{"type":"message","message":{"type":"location","id":"14743074306580","latitude":25.028028164750477,"longitude":121.5493593925288,"address":"106台灣台北市大安區敦化南路二段111號"},"timestamp":1631590614516,"source":{"type":"user","userId":"U420aa8a5b3859615a73a08c3f9fa53e2"},"replyToken":"5b7db4bb1a924978918706f76292c7a0","mode":"active"}]}';
-
-//輸出
-// error_log(); 
-// file_put_contents('php://stderr', json_encode($dataEven));
-// file_put_contents('php://stderr', $HttpRequestBody);
-
-/**
- * "events":[{
- * 						"type":"message",
- * 						"message":
- * 						{
- * 							"type":"text",
- * 							"id":"14737459099128",
- * 							"text":"..."
- * 						},
- * 						"timestamp":1631504988589,
- * 						"source":
- * 						{
- * 							"type":"user",
- * 							"userId":"U420aa8a5b3859615a73a08c3f9fa53e2"
- * 						},
- * 						"replyToken":"e38abc6740624b099206b94b25a17967",
- * 						"mode":"active"
- * 					}]
- */
