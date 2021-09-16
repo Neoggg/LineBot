a512dec718d8a10c1a20f52930a6e26b063282a5
Neoggg <thegitem@hotmail.com>
2021-09-16 17:32:38 +0800

    teest

--- lineBot.php
+++ lineBot.php
@@ -26,7 +26,7 @@ $ButtonTemplate = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplat
 	'titl',
 	'text',
 	__DIR__ . '/ellall.jpg',
-	[new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('url', 'https://open.spotify.com/track/12095GlriCNhVCbJV30vKw?si=c8713d60fa4f4a4e')],
+	[new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('url', 'https://open.spotify.com/track/12095GlriCNhVCbJV30vKw?si=c8713d60fa4f4a4e', new \LINE\LINEBot\TemplateActionBuilder\Uri\AltUriBuilder('http://example.com/pc/page/222'))],
 	'rectangle',
 	'cover',
 	'#FFFFFF'
