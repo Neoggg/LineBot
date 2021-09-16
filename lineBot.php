8ee8a1f239f6e3d97a7df7ef7f2439c1b30fdb55
Neoggg <thegitem@hotmail.com>
2021-09-16 17:09:27 +0800

    teest

--- lineBot.php
+++ lineBot.php
@@ -13,7 +13,7 @@ $jsonMessage = '{"destination":"U52d37d1d9b625754fa7308ed6af263ef","events":[{"t
 $jsonMap = '{"destination":"U52d37d1d9b625754fa7308ed6af263ef","events":[{"type":"message","message":{"type":"location","id":"14743074306580","latitude":25.028028164750477,"longitude":121.5493593925288,"address":"106台灣台北市大安區敦化南路二段111號"},"timestamp":1631590614516,"source":{"type":"user","userId":"U420aa8a5b3859615a73a08c3f9fa53e2"},"replyToken":"5b7db4bb1a924978918706f76292c7a0","mode":"active"}]}';
 
 $HttpRequestBody = file_get_contents('php://input');
-// file_put_contents('php://stderr', json_encode($HttpRequestBody));
+file_put_contents('php://stderr', json_encode($HttpRequestBody));
 $linePost = new LinePost($HttpRequestBody);
 //設定Token 
 $channelSecret =  '64f2e4b2431a448b2c872f5c58a201a9';
@@ -26,7 +26,7 @@ $ButtonTemplate = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplat
 	'title',
 	'text',
 	__DIR__ . '/ellall.jpg',
-	[new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('url', 'www.google.com')],
+	[new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('uri', 'url', 'www.google.com')],
 	'rectangle',
 	'cover',
 	'#FFFFFF'
