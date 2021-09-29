f26257fe51591633581ab049fcecd5e5069d3a10
Neoggg <thegitem@hotmail.com>
2021-09-29 10:48:39 +0800

    url

--- lineBot.php
+++ lineBot.php
@@ -21,8 +21,8 @@ $channelSecret =  '64f2e4b2431a448b2c872f5c58a201a9';
 $channelAccessToken = 'b31d8B9iAriRU9gT2b2LHKapaDFZzWga3SmlmHCMRWUsl5OplYXV/78fKWM/qjkVGX7W/ReVne/1S+9Q9Vc2bBtZsI6td4pb6sqL8MQWCNzLQPI2dh2S5tjEBN4s6+QRkFTXjCqaNTNUZYZ6F0C2cwdB04t89/1O/w1cDnyilFU=';
 // $message = $this->parserMess($HttpRequestBody);
 $message = $linePost->getMessage();
-file_put_contents('php://stderr', $message['type']);
-exit;
+// file_put_contents('php://stderr', $message['type']);
+// exit;
 
 $httpClient = new CurlHTTPClient($channelAccessToken);
 $bot = new LINEBot($httpClient, ['channelSecret' => $channelSecret]);
