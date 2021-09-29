b60950084e2410d4c33a6fec2d35a0aa7bcba7e9
Neoggg <thegitem@hotmail.com>
2021-09-29 11:46:53 +0800

    url

--- models/LineJson.php
+++ models/LineJson.php
@@ -15,14 +15,17 @@ class LineJson
       [0,0,833,843],
       [833,0,833,843],
       [1666,0,833,843],
-      // [0,843,833,843],
-      // [833,843,833,843],
-      // [1666,843,833,843],
+      [0,843,833,843],
+      [833,843,833,843],
+      [1666,843,833,843],
     ];
     $actionArr = [
       ["postback", "test"],
-      ["message", "挖金罵勒共威"],
+      ["message", "test"],
       ["uri",'url', 'https://open.spotify.com/track/1gtr662OovFOCdYjivs21N?si=51f8cf24a3e8479f'],
+      ["message", "all"],
+      ["message", "less"],
+      ["message", "no"],
     ];
     foreach ($boundArr as $key => $value) {     
       $bounds = new \LINE\LINEBot\RichMenuBuilder\RichMenuAreaBoundsBuilder($value[0],$value[1],$value[2],$value[3]);
